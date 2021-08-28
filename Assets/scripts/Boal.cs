18f6d37c158cc1361589a021daf53cabafbf5bd5
nobody <moisesalex132@gmail.com>
2021-08-28 12:17:57 -0600

    ➕ADD: velocidad y salt de la bola

--- Assets/scripts/Boal.cs
+++ Assets/scripts/Boal.cs
@@ -8,16 +8,18 @@ public class Boal : MonoBehaviour
     [SerializeField] Rigidbody2D rigibody;
     Vector2 moveDirection;
     Vector2 currentVelocity;
+    
+    [SerializeField] float speedBoall = 5;
 
     void Start()
     {
         // rigidbody2D = GetComponent<Rigidbody2D>();
-        rigibody.velocity = Vector2.up;
+        rigibody.velocity = Vector2.up * speedBoall;
     }
 
     private void FixedUpdate()
     {
-        currentVelocity = rigibody.velocity;
+        currentVelocity = rigibody.velocity ;
     }
 
     private void Update()
